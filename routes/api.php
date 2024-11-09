fac4b8dbea4fb1626f5111f2cf50f79f596898b6
CodingProper <muhamedaliwf@mail.ru>
2024-11-09 06:43:46 +0600

    4 - 00

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/V1/Categories', [\App\Http\Controllers\Api\V1\CategoryController::class.'index']);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/categories', \App\Http\Controllers\Api\V1\CategoryController::class);
+});
